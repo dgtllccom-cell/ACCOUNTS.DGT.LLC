cba9f51a3b481db52fe76ecf6eb87b79ffc9f34f
Asmatullah <dgt.llc.com@gmail.com>
2026-08-22 11:30:04 +0400

    fix: remove unused components/purchase-booking-view-redesign wrapper

--- components/purchase-booking-view-redesign.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-
-export * from "@/features/purchases/components/purchase-booking-view-redesign";
-export { default } from "@/features/purchases/components/purchase-booking-view-redesign";
